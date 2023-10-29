1b7337cd1750cbc77371c2ef0367f84ca91a69a8
diajengsekararum31@gmail.com <diajengsekararum31@gmail.com>
2023-10-29 19:30:29 +0700

    Prcobaan1, pertanyaan no5

--- ArrayBilangan07.java
+++ ArrayBilangan07.java
@@ -6,13 +6,12 @@ public class ArrayBilangan07 {
         int[] bil = new int[4];
 
         bil[0] = 5;
-        bil[1] = 13;
+        bil[1] = 12867;
         bil[2] = -7;
-        bil[3] = 17;
+        bil[3] = 200000;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+       for(int i = 0; i <= 4; i++){
+        System.out.println(bil[i]);
+       }
     }
 }
